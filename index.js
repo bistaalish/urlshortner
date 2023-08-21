7e24f8d46f41ec6ea93ddcbbe5cf85b471b32c1b
Alish Bista <bistace321@gmail.com>
2023-08-21 22:44:44 +0545

    feat: database initiated

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
+// DB set up
+const uri = process.env.MONGO_URI
+// Mongoose connect to database
+mongoose.connect(uri,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTImeoutMS: 5000
+})
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,6 +22,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(cors());
 app.use(express.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
